d03f5224cdc7825e98982622e4ecf8fa2fb56d9a
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-22 08:34:28 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Calendar;
+
+/**
+ * Created by KPS on 7/22/2020.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Calendar now = Calendar.getInstance();
+        System.out.println();
+        System.out.println("Current full date and time is : " + (now.get(Calendar.MONTH) + 1) + "-"
+                + now.get(Calendar.DATE) + "-" + now.get(Calendar.YEAR) + " "
+                + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":"
+                + now.get(Calendar.SECOND) + "." + now.get(Calendar.MILLISECOND));
+        System.out.println();
+    }
+}
